748ed2ed5ba7dc05c107c5ef56ea89e46ee6aa8b
felytic <felytic@gmail.com>
2019-04-11 15:23:48 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='time-pretty-breaks',
+    version='0.1',
+    description='Split time, date and datetime intervals into '
+    '"pretty" intervals',
+    long_description=open('README.md').read(),
+    author='Serhii Hulko',
+    author_email='felytic@gmail.com',
+    url='https://github.com/eos-vision/time_pretty_breaks',
+    packages=['time_pretty_breaks'],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: MIT',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+)
